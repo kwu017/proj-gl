a114ea5daf06c7bc93fddbc4973fa028267f33b8
Kim Wu <kimwu@ucrwpa-1-6-10-25-21-31.wnet.ucr.edu>
2019-02-25 09:36:21 -0800

    yes

---
+++ proj-gl-files/grading/driver_state.cpp
@@ -0,0 +1,216 @@
+#include "driver_state.h"
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+driver_state::driver_state()
+{
+}
+
+driver_state::~driver_state()
+{
+    delete [] image_color;
+    delete [] image_depth;
+}
+
+// This function should allocate and initialize the arrays that store color and
+// depth.  This is not done during the constructor since the width and height
+// are not known when this class is constructed.
+void initialize_render(driver_state& state, int width, int height)
+{
+    state.image_width = width;
+    state.image_height = height;
+    state.image_color = 0;
+    state.image_color = new pixel[width * height];
+    state.image_depth = 0;
+    //std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
+    for (int i = 0; i < (width * height); i++) {
+      state.image_color[i] = make_pixel(0, 0, 0);
+    }
+}
+
+// This function will be called to render the data that has been stored in this class.
+// Valid values of type are:
+//   render_type::triangle - Each group of three vertices corresponds to a triangle.
+//   render_type::indexed -  Each group of three indices in index_data corresponds
+//                           to a triangle.  These numbers are indices into vertex_data.
+//   render_type::fan -      The vertices are to be interpreted as a triangle fan.
+//   render_type::strip -    The vertices are to be interpreted as a triangle strip.
+void render(driver_state& state, render_type type)
+{
+    //std::cout<<"TODO: implement rendering. UM WAT."<<std::endl;
+    data_geometry *tri_array = new data_geometry[3];
+    float *p = state.vertex_data;
+    data_vertex in{};
+
+    switch (type){
+        case render_type::triangle:
+            for (int i = 0; i < state.num_vertices/3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    tri_array[j].data = p;
+                    p = p + state.floats_per_vertex;
+                }
+
+                for (int k = 0; k < 3; k++) {
+                    in.data = tri_array[k].data;
+                    state.vertex_shader(in, tri_array[k], state.uniform_data);
+                }
+
+                rasterize_triangle(state, (const data_geometry**) &tri_array);
+            }
+        //{
+            //for (int i = 0; i < state.num_vertices; i += 3) {
+
+                // for (int i = 0; i < state.num_vertices; i += 3) {
+                //     const data_geometry **tri_array = new const data_geometry*[3];
+            
+                //     tri_array[0] = new data_geometry;
+                //     tri_array[1] = new data_geometry;
+                //     tri_array[2] = new data_geometry;
+
+
+                //     const_cast<data_geometry*>(tri_array[0])->data = new float[MAX_FLOATS_PER_VERTEX];
+                //     const_cast<data_geometry*>(tri_array[1])->data = new float[MAX_FLOATS_PER_VERTEX];
+                //     const_cast<data_geometry*>(tri_array[2])->data = new float[MAX_FLOATS_PER_VERTEX];
+                //     for (int j = 0; j < state.floats_per_vertex; j++) {
+                //         tri_array[0]->data[j] = state.vertex_data[j + (state.floats_per_vertex * i)];
+                //         tri_array[1]->data[j] = state.vertex_data[j + (state.floats_per_vertex * (i + 1))];
+                //         tri_array[2]->data[j] = state.vertex_data[j + (state.floats_per_vertex * (i + 2))];
+                //     }
+                //     rasterize_triangle(state, tri_array);
+
+                //     delete [] tri_array[0]->data;
+                //     delete [] tri_array[1]->data;
+                //     delete [] tri_array[2]->data;
+
+                //     delete tri_array[0];
+                //     delete tri_array[1];
+                //     delete tri_array[2];
+
+                //     delete [] tri_array;
+                // }
+        break;
+        //}
+
+        case render_type::indexed:
+        break;
+
+        case render_type::fan:
+        break;
+
+        case render_type::strip:
+        break;
+
+        default:
+        break;
+    }
+}
+
+
+// This function clips a triangle (defined by the three vertices in the "in" array).
+// It will be called recursively, once for each clipping face (face=0, 1, ..., 5) to
+// clip against each of the clipping faces in turn.  When face=6, clip_triangle should
+// simply pass the call on to rasterize_triangle.
+void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
+{
+    if(face==6)
+    {
+        rasterize_triangle(state, in);
+        return;
+    }
+    std::cout<<"TODO: implement clipping. (The current code passes the triangle through without clipping them.)"<<std::endl;
+    clip_triangle(state,in,face+1);
+}
+
+// Rasterize the triangle defined by the three vertices in the "in" array.  This
+// function is responsible for rasterization, interpolation of data to
+// fragments, calling the fragment shader, and z-buffering.
+void rasterize_triangle(driver_state& state, const data_geometry* in[3])
+{
+    //std::cout<<"TODO: implement rasterization"<<std::endl;
+    int i, j;
+    int x[3], y[3];
+    unsigned image_index;
+    float area_abc;
+    float alpha, beta, gamma;
+    
+    for (unsigned k = 0; k < 3; k++) {
+        i = static_cast<int>((state.image_width / 2) * (*in)[k].gl_Position[0] + ((state.image_width / 2) - 0.5));
+        j = static_cast<int>((state.image_height / 2) * (*in)[k].gl_Position[1] + ((state.image_height / 2) - 0.5));
+
+        x[k] = i;
+        y[k] = j;
+        
+        image_index = i + j * state.image_width;
+        state.image_color[image_index] = make_pixel(255, 255, 255);
+        
+    }
+    
+    area_abc = (0.5f * ((x[1] * y[2] - x[2] * y[1]) - (x[0] * y[2] - x[2] * y[0]) - (x[0] * y[1] - x[1] * y[0])));
+    
+    for (int j = 0; j < state.image_height; j++) {
+        for (int i = 0; i < state.image_width; i++) {
+            alpha = (0.5f * ((x[1] * y[2] - x[2] * y[1]) + (y[1] - y[2]) * i + (x[2] - x[1]) * j)) / area_abc;
+            beta = (0.5f * ((x[2] * y[0] - x[0] * y[2]) + (y[2] - y[0]) * i + (x[0] - x[2]) * j)) / area_abc;
+            gamma = (0.5f * ((x[0] * y[1] - x[1] * y[0]) + (y[0] - y[1]) * i + (x[1] - x[0]) * j)) / area_abc;
+        
+            if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+                image_index = i + j * state.image_width;
+                state.image_color[image_index] = make_pixel(255, 255, 255);
+            }
+        }
+    }
+    // data_geometry* out = new data_geometry[3];
+    // int i, j;
+    // unsigned image_index;
+    // int ax, ay, bx, by, cx, cy;
+    // int px, py;
+    // float area_abc, area_pbc, area_apc, area_abp;
+    // float alpha, beta, gamma;
+  
+    // data_vertex ver;
+  
+    // for (int k = 0; k < 3; k++) {
+    //     ver.data = in[k]->data;
+
+    //     state.vertex_shader(ver, out[k], state.uniform_data);
+
+    //     out[k].gl_Position[0] /= out[k].gl_Position[3];
+    //     out[k].gl_Position[1] /= out[k].gl_Position[3];
+  
+    //     i = state.image_width/2 * out[k].gl_Position[0] + state.image_width/2 - (0.5);
+    //     j = state.image_height/2 * out[k].gl_Position[1] + state.image_height/2 - (0.5);
+
+    //     image_index = i + j * state.image_width;
+    //     state.image_color[image_index] = make_pixel(255, 255, 255);
+    // }
+
+    // ax = (state.image_width/2)*out[0].gl_Position[0] + (state.image_width/2) - (0.5);
+    // ay = (state.image_height/2)*out[0].gl_Position[1] + (state.image_height/2) - (0.5);
+    // bx = (state.image_width/2)*out[1].gl_Position[0] + (state.image_width/2) - (0.5);
+    // by = (state.image_height/2)*out[1].gl_Position[1] + (state.image_height/2) - (0.5);
+    // cx = (state.image_width/2)*out[2].gl_Position[0] + (state.image_width/2) - (0.5);
+    // cy = (state.image_height/2)*out[2].gl_Position[1] + (state.image_height/2) - (0.5);
+ 
+    // area_abc = 0.5 * (((bx * cy) - (cx * by))-((ax * cy) - (cx * ay)) - ((ax * by)-(bx * ay)));
+
+    // for (px = 0; px < state.image_width; px++) {
+    //     for (py = 0; py < state.image_height; py++) {
+    //         area_pbc = 0.5 * (((bx * cy) - (cx * by)) + ((by - cy) * px) + ((cx - bx) * py));
+    //         area_apc = 0.5 * (((ax * cy) - (cx * ay)) + ((cy - ay) * px) + ((ax - cx) * py));
+    //         area_abp = 0.5 * (((ax * by) - (bx * ay)) + ((ay - by) * px) + ((bx - ax) * py));        
+
+    //         alpha = area_pbc/area_abc;
+    //         beta = area_apc/area_abc;
+    //         gamma = area_abp/area_abc;
+
+    //         image_index = px + py * state.image_width;
+
+    //         if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+    //             state.image_color[image_index] = make_pixel(255, 255, 255);
+    //         }
+    //     }   
+    // }
+}
+
